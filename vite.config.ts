682b931c68e7953a53c847cec5bcb28bbe113e53
Noah Lange <noahrlange@gmail.com>
2026-01-18 13:33:00 -0600

    Updates for github pages.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { outDir: "pages" },
   plugins: [react(), tsconfigPaths(), tailwindcss()],
 });
